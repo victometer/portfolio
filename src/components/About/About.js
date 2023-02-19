bab3c1ab52c6b7bf7fa54290f5f9936da119e6b3
Victoria <vicki_ng92@yahoo.com>
2023-02-19 10:49:18 +0000

    added github links and css fixes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,6 +6,7 @@ const About = () => {
     return (
         <section>
             <h2 className="about__heading" id="About">About Me</h2>
+            <div className="aboutContainer">
             <p className="about__body">
             Hi there! I'm Victoria, a recent graduate of CodeClan and a newcomer to the tech industry. I'm excited to apply my newly acquired skills towards solving real-world problems and building innovative solutions."
                 
@@ -17,6 +18,7 @@ const About = () => {
         width="300"
         height="350"
     />
+    </div>
 
         </section>
     )
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,59 +10,71 @@ const Projects = () => {
         <section className="projects-all">
             <h2 className="projects" id="Projects">Projects</h2>
             <div className="project-imgages">
-                <img src={QuizzJS} alt="Quiz-image" width="300" height="330" className="project-img"/>
-                <img src={GymPython} alt="Gym-image" width="300" height="330" className="project-img" />
-                <img src={WorldJS} alt="World-image" width="300" height="330" className="project-img" />
+                <div className="grid-item">
+                    <img src={QuizzJS} alt="Quiz-image" width="300" height="330" className="project-img" />
+                    <a className="gitHubLink"
+                        href="https://github.com/victometer/Week7_Weekend_Quiz_Project">GitHub Link</a>
+                </div>
+                <div className="grid-item">
+                    <img src={GymPython} alt="Gym-image" width="300" height="330" className="project-img" />
+                    <a className="gitHubLink"
+                        href="https://github.com/victometer/Final_Project">GitHub Link</a>
+                </div>
+                <div className="grid-item">
+                    <img src={WorldJS} alt="World-image" width="300" height="330" className="project-img" />
+                    <a className="gitHubLink"
+                        href="https://github.com/victometer/Javascript-Project">GitHub Link</a>
+                </div>
             </div>
             <h2>Tech Stack</h2>
 
             <div className="tech-stack">
-            <h3>Programming Languages: </h3>
-            
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" alt="Python logo" className="logo"/> 
-            
+                <h3>Programming Languages: </h3>
 
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="JavaScript logo" className="logo" />
-            
-    
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg"
-            alt="Java logo" className="logo"/>
-            
-            
-
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt="CSS logo" className="logo"/>
-            
-
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt="HTML logo" className="logo" />
-            
-
-            <h3>Databases:</h3>
-
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" alt="PostgreSQL logo" className="logo"/>
-
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="MongoDB logo" className="logo" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" alt="Python logo" className="logo" />
 
 
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain-wordmark.svg" alt="Firebase logo" className="logo"/>
-    
-        
-            
-            <h3>Tools and Frameworks:</h3>
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="JavaScript logo" className="logo" />
 
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original-wordmark.svg" alt="Flask logo" className="logo"/>
 
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" alt="NPM logo" className="logo" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg"
+                    alt="Java logo" className="logo" />
 
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg" alt="Mocha logo" className="logo" />
 
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="React logo" className="logo" />
 
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg" alt="NodeJS logo" className="logo" />
-            
-        
-        
-        </div>
-    </section>
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt="CSS logo" className="logo" />
+
+
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt="HTML logo" className="logo" />
+
+
+                <h3>Databases:</h3>
+
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" alt="PostgreSQL logo" className="logo" />
+
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="MongoDB logo" className="logo" />
+
+
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain-wordmark.svg" alt="Firebase logo" className="logo" />
+
+
+
+                <h3>Tools and Frameworks:</h3>
+
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original-wordmark.svg" alt="Flask logo" className="logo" />
+
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" alt="NPM logo" className="logo" />
+
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg" alt="Mocha logo" className="logo" />
+
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="React logo" className="logo" />
+
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg" alt="NodeJS logo" className="logo" />
+
+
+
+            </div>
+        </section>
 
     )
 }
